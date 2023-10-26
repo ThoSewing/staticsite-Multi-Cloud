839766d9d1ca7f1a7c5fda6e3004f2af4895d7e3
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-25 21:18:12 -0300

    Infra Multi Cloud

--- terraform/azure/provider.tf
+++ terraform/azure/provider.tf
@@ -6,8 +6,8 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "rg-staticsite-tf"
-    storage_account_name = "staticsitekbtf001"
+    resource_group_name  = "rg-staticsite-tf-tom1"
+    storage_account_name = "staticsitetstf001"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
